34e0f32960764dad7eae436918ce78a38119c787
Панфилов Александр Эдуардович <panfilov@kti.ru>
2023-06-03 23:51:08 +0300

    Добавьте файлы проекта.

---
+++ lr3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace lr3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, Rad;
+
+            Console.WriteLine("1. Прямоугольник");
+            Console.WriteLine("2. Круг");
+            //Console.WriteLine("3. Треугольник");
+            //Console.WriteLine("4. Трапеция");
+            //Console.WriteLine("5. Сектор");
+
+            string temp = Console.ReadLine();
+            int variant = Convert.ToInt32(temp);    // преобразование в число
+
+            switch (variant)
+            {
+                case 1:
+                    Console.Write("Сторона А = ");
+                    temp = Console.ReadLine();
+                    a = Convert.ToDouble(temp);
+                    Console.Write("Сторона B = ");
+                    temp = Console.ReadLine();
+                    b = Convert.ToDouble(temp);
+                    Console.WriteLine("Площадь прямоугольника = " + a * b);
+                    break;
+
+                case 2:
+                    Console.Write("Радиус R = ");
+                    temp = Console.ReadLine();
+                    Rad = Convert.ToDouble(temp);
+                    Console.WriteLine("Площадь круга = " + 4 * Atan(1.0) * Pow(Rad, 2.0));
+                    break;
+
+                default: Console.WriteLine("Выбор неверен "); break;
+            }
+        }
+    }
+}
